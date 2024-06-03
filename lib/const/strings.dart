e3a3dfc55423a6d28b51aa06c8df9e0abecdd086
Erhabor-Fona <osaofonaerhabor@gmail.com>
2024-06-03 01:41:44 +0100

    [New]--updated strings class

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -1,3 +1,7 @@
 class Strings{
-
+static const String enterName = 'Enter your name';
+static const String start = 'Start';
+static const String next = 'Next';
+static const String previous = 'Previous';
+static const String leaderBoard = 'LeaderBoard';
 }
