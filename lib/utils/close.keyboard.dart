ec704def9f5d5a7f89bd2d582665c5343297f231
Erhabor-Fona <osaofonaerhabor@gmail.com>
2024-06-03 02:24:37 +0100

    [New]--updated printing util and close keyboard util

---
+++ lib/utils/close.keyboard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+
+class KeyboardUtil {
+  static void hideKeyboard(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+      FocusManager.instance.primaryFocus?.unfocus();
+    }
+  }
+}
+
+class CloseKeyboard extends StatelessWidget {
+  final Widget child;
+  const CloseKeyboard({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        KeyboardUtil.hideKeyboard(context);
+      },
+      child: child,
+    );
+  }
+}
---
+++ lib/utils/out.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/foundation.dart';
+
+void out (Object? obj){
+  if (kDebugMode) print(obj);
+}
